ee7af9f5541d2ecd7d4b6cf7efd234f932a20cc1
Default Sobriquet <51099220+DefaultSobriquet@users.noreply.github.com>
2020-06-19 08:28:15 -0700

    Feat: Add fox

---
+++ src/modules/Entertainment/fox.ts
@@ -0,0 +1,37 @@
+import axios from "axios";
+import {MessageEmbed} from "discord.js";
+import {Command} from "../../structures/Command";
+import {ICommandContext} from "../../types";
+import IllustraClient from "../../structures/IllustraClient";
+
+const options: Partial<Command> = {
+    name: "fox",
+    description: "Floof? Find a fox!",
+    module: "Entertainment",
+    usage: "",
+    examples: [""],
+    aliases: ["meow"],
+    userPerms: [],
+    botPerms: ["SEND_MESSAGES", "EMBED_LINKS"]
+};
+
+class Fox extends Command{
+	constructor(){
+		super(options);
+	}
+	async execute(ctx: ICommandContext, Illustra: IllustraClient): Promise<void>{
+
+		const request = (await axios.get("https://randomfox.ca/floof/")).data;
+		
+		const embed = new MessageEmbed()
+			.setTitle("Floof!")
+			.setImage(request.image)
+			.setColor(ctx.guild!.me!.displayColor || 0x2f3136)
+			.setTimestamp()
+			.setFooter(`Requested by ${ctx.user.tag} • Powered by randomfox.ca`);
+		
+		ctx.channel.send(embed);
+	}
+}
+
+export default Fox;
